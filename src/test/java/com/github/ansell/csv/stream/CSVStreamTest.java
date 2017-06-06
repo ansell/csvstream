e5b562f6730580a036b6f064977bdde64347f973
Peter Ansell <p_ansell@yahoo.com>
2017-06-06 14:26:14 +1000

    issue #3 : Test comments using hash character
    Signed-off-by: Peter Ansell <p_ansell@yahoo.com>

--- src/test/java/com/github/ansell/csv/stream/CSVStreamTest.java
+++ src/test/java/com/github/ansell/csv/stream/CSVStreamTest.java
@@ -697,4 +697,39 @@ public class CSVStreamTest {
 		assertFalse("Too many lines", lineError.get());
 	}
 
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.csv.util.CSVStream#parse(java.io.Reader, java.util.function.Consumer, java.util.function.BiFunction, java.util.function.Consumer, List, int)}
+	 * .
+	 */
+	@Test
+	public final void testStreamCSVComment() throws Exception {
+		
+		AtomicBoolean headersGood = new AtomicBoolean(false);
+		AtomicBoolean lineGood = new AtomicBoolean(false);
+		AtomicBoolean foundLine = new AtomicBoolean(false);
+		AtomicBoolean lineError = new AtomicBoolean(false);
+		// The default mapper skips comment lines
+		CsvMapper mapper = CSVStream.defaultMapper();
+		CsvSchema schema = CsvSchema.builder().setQuoteChar('\'').build();
+		CSVStream.parse(new StringReader("'Test''\nHeader1'\n#A Comment that should be skipped\n'TestValue1'"), h -> {
+			if (h.size() == 1 && h.contains("Test'\nHeader1")) {
+				headersGood.set(true);
+			}
+		}, (h, l) -> {
+			if (foundLine.compareAndSet(false, true) && l.size() == 1 && l.contains("TestValue1")) {
+				lineGood.set(true);
+			} else {
+				lineError.set(true);
+			}
+			return l;
+		}, l -> {
+		}, null, 1, mapper, schema);
+
+		assertTrue("Headers were not recognised", headersGood.get());
+		assertTrue("Line was not recognised", lineGood.get());
+		assertTrue("Line was not found", foundLine.get());
+		assertFalse("Too many lines", lineError.get());
+	}
+
 }
